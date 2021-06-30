ada9d0db7da335ebf6d83c95bec20b28839b78e0
Hagen Carstensen <hagen.carstensen@porsche.de>
2021-06-30 14:50:29 +0200

    solve

--- 1.12-1.14/src/App.js
+++ 1.12-1.14/src/App.js
@@ -10,11 +10,18 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
   ]
+  const points = Array(anecdotes.length).fill(0)
 
   const [selected, setSelected] = useState(0)
+  const [voted, setVote] = useState(points)
 
   const handleClick = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+  const handleVote = () =>{
+    const newVote = [...voted];
+    newVote[selected] += 1;
+    setVote(newVote);
   }
 
   
@@ -24,7 +31,11 @@ const App = () => {
       <div>
         {anecdotes[selected]}
       </div>
+      <div>     
+        HAT {voted[selected]} VOTES
+      </div>
       <div>
+        <button onClick={handleVote}>vote</button>
         <button onClick={handleClick}>Next quote</button>
       </div>
     </div>
